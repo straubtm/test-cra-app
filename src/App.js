26fff8b070ed9f734f0496ea785edebb2bdc5c80
straubtm <tstraub@healthadvocate.net>
2020-04-07 15:29:48 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1121,w_2000,x_0,y_183/f_auto,q_auto,w_1100/v1554734515/shape/mentalfloss/567954-walt_disney_productions.jpg" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <div>
+          supercalifragellesteckexpaladocious
+          </div>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
